bdcdb22f9ea952450a45f1a36f616d29714231fe
Brett Parsons <parsonsbrett0@bretts-mbp.myfiosgateway.com>
2020-03-20 15:42:35 -0400

    basic setup complete. ready to start building out dashboard content

--- app.py
+++ app.py
@@ -23,12 +23,85 @@ sidebar = html.Div(
         html.Div(
             id = "sidebar-content",
             children = [
-                html.H2("Sidebar", className="display-4"),
-                html.Hr(),
-                html.P(
-                    "A sidebar with collapsible navigation links", className="lead"
+                html.Div(
+                    id = "thermographs",
+                    className = "tab tabOption menuItem tabSelected",
+                    children = [
+                        html.Div(className = "tabHighlight"),
+                        html.Div(
+                            className = "tabLabel", 
+                            children = [
+                                html.I(className="tabIcon fas fa-fire"),
+                                "Thermographs"
+                            ]
+                        )
+                    ]
+                ),
+                html.Div(
+                    id = "viewData",
+                    className = "tab tabOption menuItem",
+                    children = [
+                        html.Div(className = "tabHighlight"),
+                        html.Div(
+                            className = "tabLabel", 
+                            children = [
+                                html.I(className="tabIcon fas fa-th"),
+                                "View Data"
+                            ]
+                        )
+                    ]
+                ),
+                html.Div(
+                    id = "accessData",
+                    className = "tab tabOption menuItem",
+                    children = [
+                        html.Div(className = "tabHighlight"),
+                        html.Div(
+                            className = "tabLabel", 
+                            children = [
+                                html.I(className="tabIcon fas fa-server"),
+                                "Access Data"
+                            ]
+                        )
+                    ]
+                ),
+                html.Div(
+                    id = "documentation",
+                    className = "tab submenuTab menuItem",
+                    children = [
+                        html.Div(className = "tabHighlight"),
+                        html.Div(
+                            className = "tabLabel", 
+                            children = [
+                                html.I(className="tabIcon fas fa-file"), 
+                                "Documentation",
+                                html.I(className="submenuTabIcon fas fa-angle-down"),
+                            ]
+                        )
+                    ]
+                ),
+                # Sub menu content
+                dbc.Collapse(
+                    id = "documentationSubmenu",
+                    className = "submenu",
+                    children = [
+                        html.Div(
+                            id = "userGuide",
+                            className = "tabOption menuItem submenuItem",
+                            children = html.Div([html.I(className="tabIcon fas fa-angle-double-right"), "User Guide"])
+                        ),
+                        html.Div(
+                            id = "readme",
+                            className = "tabOption menuItem submenuItem",
+                            children = html.Div([html.I(className="tabIcon fas fa-angle-double-right"), "README"])
+                        ),
+                        html.Div(
+                            id = "contributing",
+                            className = "tabOption menuItem submenuItem",
+                            children = html.Div([html.I(className="tabIcon fas fa-angle-double-right"), "CONTRIBUTING"])
+                        )
+                    ]
                 )
-                # dbc.Nav(submenu_1 + submenu_2, vertical=True)
             ]
         )
     ]
@@ -37,11 +110,85 @@ sidebar = html.Div(
 navbar = html.Div(
     id = "navbar",
     children = [
-        html.I(id = "sidebar-btn", className="fa fa-bars fa-lg")
+        html.I(id = "sidebar-btn", className="fa fa-bars fa-lg"),
+        html.Div(
+            id = "notifications",
+            children = [
+                html.Div(   
+                    id = "notificationIconRow",                  
+                    children = html.I(className="notificationIcon fas fa-envelope")
+                ),
+                html.Div(
+                    id = "notificationsDropdowns",
+                    children = [
+                        html.Div(
+                            id = "messagesDropdown", 
+                            className = "notificationDropdwon",
+                            children = [
+                                html.Div(
+                                    className = "notificationTitle",
+                                    children = "Messages"
+                                ),
+                                html.Div(
+                                    className = "notificationContent",
+                                    children = "you got them messages. check em out. read em up. get em done."
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ]
+        )
     ]
 )
 
-content = html.Div(id="page-content")
+content = html.Div(
+    id = "page-content",
+    children = [
+        html.Div(
+            id = "thermographs-content",
+            className = "tabContent contentSelected",
+            children = [
+                "This is your thermograph dashboard"
+            ]
+        ),
+        html.Div(
+            id = "viewData-content",
+            className = "tabContent",
+            children = [
+                "This is your data table"
+            ]
+        ),
+        html.Div(
+            id = "accessData-content",
+            className = "tabContent",
+            children = [
+                "This is your access panel"
+            ]
+        ),
+        html.Div(
+            id = "userGuide-content",
+            className = "tabContent",
+            children = [
+                "Guide thy user."
+            ]
+        ),
+        html.Div(
+            id = "readme-content",
+            className = "tabContent",
+            children = [
+                "I am a readme. Read me."
+            ]
+        ),
+        html.Div(
+            id = "contributing-content",
+            className = "tabContent",
+            children = [
+                "Contribute."
+            ]
+        )
+    ]
+)
 
 mainbar = html.Div(
     id = "mainbar",
@@ -49,6 +196,19 @@ mainbar = html.Div(
 )
 
 app.layout = html.Div([dcc.Location(id="url"), sidebar, mainbar])
+
+
+
+@app.callback(
+    Output("documentationSubmenu", "is_open"),
+    [Input("documentation", "n_clicks")],
+    [State("documentationSubmenu", "is_open")],
+)
+def toggle_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
 
 
 if __name__ == "__main__":
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,45 @@
 $( window ).on( "load", function() {
     console.log("jquery working");
+
+    // Operates the sliding sidebar
     $("#sidebar-btn").click(function(){
         $("#mainbar").toggleClass("mainbar-marginLeft");
     });
+
+    // Toggle viewing the messages
+    $("#notificationIconRow").click(function(){
+        $("#notificationsDropdowns").toggleClass("notificationsVisible");
+    });
+    
+
+    // Controls what gets displayed in the page-content
+    $(".tabOption").click(function(el) {
+        $(".contentSelected").removeClass("contentSelected");
+        $("#" + $(el)[0].currentTarget.id + "-content")[0].classList.add("contentSelected")
+    });
+    
+    // Controls the sidebar functionality during navigation
+    $(".tabOption").click(function(el) {
+        $(".tabSelected").removeClass("tabSelected");
+        $(".submenuItemSelected").removeClass("submenuItemSelected");
+
+        // is it a tab
+        if (el.currentTarget.classList.contains('tab') === true) {
+            // tab
+            $(el)[0].currentTarget.classList.add("tabSelected");
+        } else {
+            // submenu item
+            
+            // first make parent of sub menu item the selected tab
+            $("#" + $(el)[0].currentTarget.parentElement.previousElementSibling.id)[0].classList.add("tabSelected");
+            
+            // highlight the submenu item
+            $(el)[0].currentTarget.classList.add('submenuItemSelected');
+        }
+    });
+
+    $(".submenuTab").click(function(el) {
+        $("#" + $(el)[0].currentTarget.id + " .submenuTabIcon").toggleClass("fa-angle-down fa-angle-up");
+    });
+
 });
